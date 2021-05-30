d3777d652b14f706040f2d201065014509a5074c
e-boi <hiboysismyoufake6789@gmail.com>
2021-05-30 16:10:40 -0400

    add this to cooldown too

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { readdirSync, readFileSync } from 'fs';
 Client.commands = new Discord.Collection();
 Client.aliases = new Discord.Collection();
 Client.categories = readdirSync('./src/commands/');
+Client.cooldowns = new Map();
 
 import('./handlers/command.js').then(file => file.default(Client));
 import('./handlers/event.js').then(file => file.default(Client));
