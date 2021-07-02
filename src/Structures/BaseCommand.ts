91313a0fd3c1820d6a61d0380ded30c89cbc3226
e-boi <hiboysismyoufake6789@gmail.com>
2021-07-02 17:27:25 -0400

    add logger to commands

--- src/Structures/BaseCommand.ts
+++ src/Structures/BaseCommand.ts
@@ -1,4 +1,5 @@
 import { MessageEmbedOptions, PermissionResolvable } from 'discord.js';
+import Logger from './Logger';
 
 interface CommandProps {
 	name: string;
@@ -16,7 +17,9 @@ export type MessageReturn = string | { embed: MessageEmbedOptions };
 export default class BaseCommand {
 	run: (...args: any[]) => Promise<MessageReturn> | MessageReturn;
 	help: CommandProps;
+	logger: Logger;
 	constructor({ name, description, usage, aliases, nsfw }: CommandProps) {
 		this.help = { name, description, usage, aliases, nsfw };
+		this.logger = new Logger(`command: ${this.help.name}`);
 	}
 }
