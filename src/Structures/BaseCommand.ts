2f5bb95a431f088c6f2373743128c859aa83dfcc
e-boi <hiboysismyoufake6789@gmail.com>
2021-07-03 16:14:17 -0400

    have help and configs for a command 2 diff things

--- src/Structures/BaseCommand.ts
+++ src/Structures/BaseCommand.ts
@@ -13,14 +13,31 @@ interface CommandProps {
 	guildOnly?: boolean;
 }
 
+interface helpProps {
+	name: CommandProps['name'];
+	description: CommandProps['description'];
+	usage: CommandProps['usage'];
+	aliases?: CommandProps['aliases'];
+}
+
+interface configProps {
+	nsfw: CommandProps['nsfw'];
+	botPerms: CommandProps['botPerms'];
+	userPerms: CommandProps['userPerms'];
+	guildOnly?: CommandProps['guildOnly'];
+	devOnly?: CommandProps['devOnly'];
+}
+
 export type MessageReturn = string | { embed: MessageEmbedOptions };
 
 export default class BaseCommand {
 	run: (...args: any[]) => Promise<MessageReturn> | MessageReturn;
-	help: CommandProps;
+	help: helpProps;
+	config: configProps;
 	logger: Logger;
-	constructor({ name, description, usage, aliases, nsfw, guildOnly }: CommandProps) {
-		this.help = { name, description, usage, aliases, nsfw, guildOnly };
+	constructor({ name, description, usage, aliases, nsfw, guildOnly, botPerms, userPerms, devOnly }: CommandProps) {
+		this.help = { name, description, usage, aliases };
+		this.config = { nsfw, guildOnly, botPerms, userPerms, devOnly };
 		this.logger = new Logger(`command: ${this.help.name}`);
 	}
 }
--- src/events/message.ts
+++ src/events/message.ts
@@ -16,10 +16,10 @@ export default class message {
 
 		const command = this.client.commands.get(cmd) || this.client.commands.get(this.client.aliases.get(cmd) || '');
 		if (!command) return;
-		if (command.help.guildOnly && message.channel.type === 'dm') return message.channel.send('Try this command in a server.');
-		else if (command.help.nsfw && message.channel.type === 'text' && !message.channel.nsfw)
+		if (command.config.guildOnly && message.channel.type === 'dm') return message.channel.send('Try this command in a server.');
+		else if (command.config.nsfw && message.channel.type === 'text' && !message.channel.nsfw)
 			return message.channel.send("Can't use nsfw command in a not nsfw channel.");
-		else if (command.help.devOnly && config.ownerId.includes(message.author.id)) return message.channel.send("Can't use a dev only command.");
+		else if (command.config.devOnly && config.ownerId.includes(message.author.id)) return message.channel.send("Can't use a dev only command.");
 		const res = await command.run(message, args, this.client);
 		return message.channel.send(res);
 	}
